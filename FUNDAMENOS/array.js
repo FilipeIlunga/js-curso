35b0a5ba74df495dbbab8312401fe6dfce930df3
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-04 18:20:45 -0300

    Array1

--- FUNDAMENOS/array.js
+++ FUNDAMENOS/array.js
@@ -1,8 +1,11 @@
 const valores = [1, 2, 3, 4, 5, 6, 7, 8]
 
-function teste(valor) {
-    return valor <=5
-}
+valores[8] = 9
+console.log(valores)
 
-
-console.log(valores.filter(teste))
+valores.push(10)
+console.log(valores)
+valores.pop()
+console.log(valores)
+delete valores[0]
+console.log(valores)
