a8e62aa658772f3274b28e4f0dfaa0791239b1c0
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-03 23:02:48 -0300

    String

---
+++ FUNDAMENOS/string.js
@@ -0,0 +1,26 @@
+const escola = "F1l3pe ilunga xindanhi"
+
+console.log("Quinta palavra: " + escola.charAt(4))
+console.log("Código ascii da quarta palavra " + escola.charAt(3) + " - " + escola.charCodeAt(3))
+console.log("Imprimir do indice 1 ate o final: " + escola.substring(1))
+console.log('Substitui todos os numeros por "e": ' + escola.replace(/\d/g, 'e'))
+console.log('Substitui todos os l por "x": ' + escola.replace('l', 'x'))
+console.log('Transforma a string em lista de palavras: ' + escola.split(' '))
+
+function quantasVezesAperece(texto, palavra) {
+    let textos = texto.split(/(?:,| )+/)
+    let cont =0
+    for (let i = 0; i < textos.length; i++) {
+        if (textos[i] == palavra) {
+            cont++
+        }
+    }
+    console.log("\n\n\ A palavra " +palavra+" apararece "+cont+" vezes no texto")
+};
+
+// Dado um texto, retornar quantas vezes uma palavra aparece no texto
+let texto = "O pe do pedro é preto, mas o pedro é branco e preto"
+let texto1 = "Saberás que não te amo e que te amo posto que de dois modos é a vida  a palavra é uma asa do silêncio, o fogo tem uma metade de frio. Eu te amo para começar a amar-te,"
+
+quantasVezesAperece(texto1, 'é')
+
