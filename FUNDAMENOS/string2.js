445e7097426017b7b899d5d5525100a6c89163a7
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-03 23:26:27 -0300

    String template 2

---
+++ FUNDAMENOS/string2.js
@@ -0,0 +1,11 @@
+const nome = 'Rebeca'
+const concatenacao = 'ola ' + nome + '!'
+const template = ` Olá ${nome}!`
+
+console.log(concatenacao, template)
+//expressões...
+console.log(`1+1=${1 + 1}`)
+
+const up = s => s.toUpperCase()
+
+console.log(`Nome ${nome} em maisculo: ${up(nome)}`)
