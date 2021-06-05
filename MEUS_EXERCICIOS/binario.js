56a5cbd56e3f8693a0654ad9ca0202922aa5c896
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-05 14:42:45 -0300

    Aritmetica

--- MEUS_EXERCICIOS/binario.js
+++ MEUS_EXERCICIOS/binario.js
@@ -1,19 +1,22 @@
 
-for (numero = 0; numero <= 100; numero++){
+ for (numero = 0; numero <= 100; numero++){
     controlador = numero
+    
     bin = []
     if (numero != 0) {
         while (controlador != 1) {  
             bin.push((controlador % 2))
-            controlador = (controlador/2)|0
+            controlador = parseInt(controlador/2)
         }
         bin.push(1)
-        binFinal = Number(bin.reverse().join('')) 
+        binFinal = parseInt(bin.reverse().join('')) 
     } else {
         binFinal =0
     }
     console.log(`${numero}= ${binFinal}\n`)
 }
+
+ 
 
 
 
