57db4b24861ecf4b47181d27a6c850b90efab058
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-05 21:46:55 -0300

    Shift pronto

--- MEUS_EXERCICIOS/shift.js
+++ MEUS_EXERCICIOS/shift.js
@@ -1,16 +1,25 @@
 
-function shif(texto, shifts) {
+function shifString(texto, sL,sR) {
     const array = texto.split("")
-    for (let i = 0; i < shifts; i++) {
-        let pop = array[array.length - 1]
-        array.pop()
-        array.unshift(pop)
+
+    if (sR > sL) {
+        for (let i = 0; i < (sR-sL); i++) {
+            let pop = array[array.length - 1]
+            array.pop()
+            array.unshift(pop)
+        }
+    } else{
+        for (let i = 0; i < (sL -sR); i++) {
+            let pop = array[0]
+            array.shift()
+            array.push(pop)
+        }
     }
     return array.join('')
 }
 
-let lista = "ABCDE"
-lista = shif(lista, 17)
+let lista = "ABCDEF"
+lista = shifString(lista,3,2)
 console.log(lista)
 
 
